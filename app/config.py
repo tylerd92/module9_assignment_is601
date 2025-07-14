0d6f093c0080ac205c3d832ea292e0ebc57aa5bd
tylerd92 <twdeans@gmail.com>
2025-07-14 18:29:36 -0400

    update database config set up

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
 
     class Config:
-        env_file = '.env'
+        env_file = ".env"
 
 settings = Settings()
